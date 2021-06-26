0697521faf79dd91a53bbf5d02d69f4ce25d7340
Majjcom <majjcom@126.com>
2021-06-26 15:00:42 +0800

    update

--- check.py
+++ check.py
@@ -2,6 +2,8 @@ import psutil
 import time
 import os
 
+name = 'died'
+
 run = True
 while run:
     pids = psutil.pids()
@@ -10,18 +12,14 @@ while run:
         for pid in pids:
             p = psutil.Process(pid)
             process_name = p.name()
-            #print(process_name)
             if process_name == 'aria2c.exe':
-                run = True
-        time.sleep(1)
+                name = 'got'
     except:
         run = True
-        time.sleep(1)
         pass
 
 path_real = str(__file__)
 print(path_real)
 path_name = os.path.basename(path_real)
-f = open(path_real[:path_real.find(path_name)] + 'died', 'w')
-f.write('aria2 is dead...')
+f = open(path_real[:path_real.find(path_name)] + name, 'w')
 f.close()
